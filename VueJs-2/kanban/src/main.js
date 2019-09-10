ba831163dd117f373e40b770359b8bac8f012368
WolfLudwig <[wolf_ludwig@outlook.com]>
2019-09-10 15:32:21 +0200

    Exo de changement de format

--- VueJs-2/kanban/src/main.js
+++ VueJs-2/kanban/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue"     //se lie à App.vue
 
 Vue.config.productionTip = false; //met la fonctionnalité de prod
 
-let app = new Vue({               //permet liaison de l'app
+new Vue({               //permet liaison de l'app
   render: h => h(App),
 }).$mount("#app")
 
