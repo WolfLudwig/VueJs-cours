bdfb7893e35f6ddb9053222afa9c8882bdf8da33
WolfLudwig <[wolf_ludwig@outlook.com]>
2019-09-10 14:24:17 +0200

    Mise à neuf kanban

--- VueJs-2/kanban/src/main.js
+++ VueJs-2/kanban/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue'           //importe la librairie Vue
+import App from "./App.vue"     //se lie à App.vue
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false; //met la fonctionnalité de prod
 
-new Vue({
+let app = new Vue({               //permet liaison de l'app
   render: h => h(App),
-}).$mount('#app')
+}).$mount("#app")
+
+console.log(Vue)
